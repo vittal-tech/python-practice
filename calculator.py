22a1b7ac69d009af1d5858837176fc697adb72da
vittal-tech <nagavenibmn@gmail.com>
2026-03-06 16:24:56 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,7 @@
+num1 = int(input("enetr your first number :"))
+num2 = int(input("enter your second number:"))
+
+print("addition : " , num1 + num2)
+print("subtraction :" , num1 - num2)
+print("multiplication : " , num1*num2)
+print("division :" , num1/num2)
